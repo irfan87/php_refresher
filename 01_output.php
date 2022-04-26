76be98fe86f0c977cf2d4fb7ea1acd255857a2a3
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-04-27 01:32:06 +0800

    01_output.php

--- 01_output.php
+++ 01_output.php
@@ -1,2 +1,15 @@
 <?php
-echo "hello world!";
+// echo - Output strings, numbers, html, etc
+echo 123, "name";
+
+// print() - work like echo, but can only take in a single argument
+print("hello world");
+
+// print_r() = print single values and arrays
+print_r([1, 2, 3, 4]);
+
+// var_dump - returns more info like data types and length
+var_dump('Hello');
+
+// var_export() - similar to var_dump(). Outputs a string representation of variables
+var_export("hello");
